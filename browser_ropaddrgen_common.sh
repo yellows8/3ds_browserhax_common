32503809c5b6baf28f5d6be6d0c501ac93b7ff5f
yellows8 <yellows8@users.noreply.github.com>
2015-09-12 01:44:37 -0400

    Added ROP_STR_R0TOR1 to the common rop-gen script.

--- browser_ropaddrgen_common.sh
+++ browser_ropaddrgen_common.sh
@@ -1,1 +1,3 @@
 ropgadget_patternfinder $1/*exefs/code.bin --script=browser_ropgadget_script --baseaddr=0x100000 --patterntype=sha256
+ropgadget_patternfinder $2/webkit.cro --patterntype=sha256 --patterndata=17f19cd8b8896468edbad52b1a47862f432c6f7b119e7f5b2f4458d3a9009795 --patternsha256size=0x8 "--plainout=\$ROP_STR_R0TOR1 = \$WEBKITCRO_MAPADR+"
+echo -n -e "\n"
--- browser_ropaddrgen_spider.sh
+++ browser_ropaddrgen_spider.sh
@@ -1,2 +1,2 @@
-./browser_ropaddrgen_common.sh $1
+./browser_ropaddrgen_common.sh $1 $1/*romfs/cro
 ropgadget_patternfinder $1/*exefs/code.bin --script=browser_ropgadget_script_spiderext --baseaddr=0x100000 --patterntype=sha256
