0ac5563ecb6e194c23ba47de29a2611c2152e3ed
yellows8 <yellows8@users.noreply.github.com>
2015-09-12 00:56:13 -0400

    Added ROP_STMR0_R0PC for skater rop-gen.

--- browser_ropaddrgen_skater.sh
+++ browser_ropaddrgen_skater.sh
@@ -1,2 +1,4 @@
 ./browser_ropaddrgen_common.sh $1
 ropgadget_patternfinder $1/*exefs/code.bin --script=browser_ropgadget_script_skaterext --baseaddr=0x100000 --patterntype=sha256
+echo -n -e "\n"
+ropgadget_patternfinder $1/peer.cro --baseaddr=0x7 --patterntype=sha256 --patterndata=6279580b19de16e57488d944436459e2dfbc8b35d704f371bc5fcec660aa4a6c --patternsha256size=0xe --stride=0x2 "--plainout=\$ROP_STMR0_R0PC = \$PEERCRO_MAPADR+"
