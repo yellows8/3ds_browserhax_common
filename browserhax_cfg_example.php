53f98036f4c2c82f845764adce80ee59b50663f6
yellows8 <yellows8@users.noreply.github.com>
2016-08-14 13:09:13 -0400

    Updated comments for the cfg example.

--- browserhax_cfg_example.php
+++ browserhax_cfg_example.php
@@ -16,7 +16,7 @@ function browserhaxcfg_handledefault()
 
 function browserhaxcfg_getbinparam_type3()
 {
-	//Return a string for the value of the "getbin" URL param for use with $getbinselect value3, which is used by browserhaxcfg_parsebinparam(). This is also the getbin URL param value used by the ROP generation code in 3dsbrowserhax_common.php for generating the arm11code payload URL(when $arm11code_loadfromsd is value 2).
+	//Return a string for the value of the "getbin" URL param for use with $getbinselect value3, which is used by browserhaxcfg_parsebinparam(). This is also the getbin URL param value used by the ROP generation code in 3dsbrowserhax_common.php for generating the arm11code payload URL(when $arm11code_loadfromsd is value 2). This is just an ID, the value doesn't matter(on the host server this is just a random ASCII string).
 	return "<someid>";
 }
 
@@ -34,13 +34,13 @@ function browserhaxcfg_parsebinparam()
 
 function browserhaxcfg_getbinpath_val3()
 {
-	//Return a filepath string which will be loaded by 3dsbrowserhax_common.php itself, when a binary is requested via the "getbin" URL param with the $getbinselect variable set to value 3 (see browserhaxcfg_parsebinparam).
+	//Return a filepath string which will be loaded by 3dsbrowserhax_common.php itself, when a binary is requested via the "getbin" URL param with the $getbinselect variable set to value 3 (see browserhaxcfg_parsebinparam). This should be the built 3ds_arm11code.bin from running 'make'.
 	return "<somefilepath>";
 }
 
 function browserhaxcfg_getbinpath_ropchain2()
 {
-	//Return a filepath string which will be loaded by 3dsbrowserhax_common.php itself, for loading arm11code which will be embedded in the ROP when $arm11code_loadfromsd==0.
+	//Return a filepath string which will be loaded by 3dsbrowserhax_common.php itself, for loading arm11code which will be embedded in the ROP when $arm11code_loadfromsd==0. With the normal configuration this isn't used.
 	return "<somefilepath>";
 }
 
